9de5065ca76f093f8df0f01ffeea1f1ed42af2f9
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-04-22 19:08:18 +0900

    Controller

---
+++ db/migrate/20190422095312_create_products.rb
@@ -0,0 +1,10 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name, null: false
+      t.integer :price, null: false
+      t.string :image, null: false
+      t.timestamps
+    end
+  end
+end
