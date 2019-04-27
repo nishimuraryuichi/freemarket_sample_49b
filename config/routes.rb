0afd8d43bfcc6fd85f031398b4fa6f9ebb96b132
ryuuuuuu1 <ryu@ryunoMacBook-Air.local>
2019-04-27 22:00:07 +0900

    Products detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'products#index'
-  resources :products, only: [:index]
-  # resources :users
-  # resources :comments
-  # resources :brands
-  # resources :category_pars
-  # resources :categorys
-  # resources :category_childs
+  resources :products
+  resources :users
+  resources :comments
+  resources :brands
+  resources :category_pars
+  resources :categorys
+  resources :category_childs
 end
