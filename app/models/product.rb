c69a186d1ebcc063b14e978310420428b32375a9
q1umi-ooki <q1umi-ooki>
2019-05-03 20:37:22 +0900

    Using ActiveHash at several Models and Related with Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ApplicationRecord
- def array
-  [["---", "value"], ["送料込み(出品者負担)", "value"], ["着払い(購入者負担)", "value"]]
- end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :parent_category, :status, :delivery_fee, :preparation
 end
