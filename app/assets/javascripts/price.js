8ad546c98143a135517fb201defe8bede65f5f20
q1umi-ooki <q1umi-ooki>
2019-05-04 18:51:03 +0900

    Add '¥' and ','

--- app/assets/javascripts/price.js
+++ app/assets/javascripts/price.js
@@ -1,13 +1,10 @@
 $(function(){
   $('#selling-price').on('keyup',function(){
     let input = $('#selling-price').val();
-    console.log(input);
     if (input >= 300) {
       let commissionPrice = Math.floor(input*0.1);
-      $('.commission__price').text(commissionPrice);
-      console.log(input);
-      console.log(input * 0.9);
-      $('.benefit__price').text(input - (commissionPrice));
+      $('.commission__price').text('¥'+ commissionPrice.toLocaleString());
+      $('.benefit__price').text('¥' + (input - (commissionPrice)).toLocaleString());
     }else{
       $('.commission__price').text('-');
       $('.benefit__price').text('-');
