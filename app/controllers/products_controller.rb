b4d10ce6afcf0a1ae32c60cb7b7b2749bb5268e6
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-05-04 16:12:06 +0900

    Commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,14 +12,15 @@ class ProductsController < ApplicationController
   end
 
   def create
-    # @product = Product.new(product_params)
-    # if @product.save?
-    #   redirect_to 
-    # else
-    #   @product = Product.new
-    #   flash.now[:alert] = "入力項目を埋めきれていません。もう一度入れ直してください"
-    #   render :new
-    # end     
+    @product = Product.new(product_params)
+    if @product.save
+        redirect_to new_product_path
+    else
+       @product = Product.new
+      #  flash.now[:alert] = "入力項目を埋めきれていません。もう一度入れ直してください"
+       render :new
+    end
+
   end
 
   def edit
@@ -38,7 +39,7 @@ class ProductsController < ApplicationController
   end
 
   private
-  # def product_params
-  #   params.require(:product).permit(:)
-  # end
+  def product_params
+    params.require(:product).permit(:name,:price,:image,:detail,:parent_category_id,:status_id,:delivery_fee_id,:prefecture_id,:preparation_id)
+  end
 end
