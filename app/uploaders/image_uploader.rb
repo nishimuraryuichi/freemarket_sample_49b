e98df5c0b718d490073bab4b974bc79beda9be28
q1umi-ooki <q1umi-ooki>
2019-05-05 14:53:51 +0900

    Add 'data-turbolinks:false' on links which access to exhibit page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,5 +44,5 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-  process resize_to_fit: [300, 300]
+  process resize_to_fit: [800, 800]
 end
