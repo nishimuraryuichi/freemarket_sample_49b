53810eb32f0e8c258b07618298664871f176fd30
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-05-07 15:29:03 +0900

    Commit

--- spec/controllers/products_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe ProductsController do
-  describe 'PRODUCT #update' do
-    it "renders the :update templete" do
-      put :update,params:{id: }
-    end
-  end
-end
