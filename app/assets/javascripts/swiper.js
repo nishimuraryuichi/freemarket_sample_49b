1178e53f3b3959b416dd2047c71cac38abd51918
shoichinishiguchi <shoichi2228@yahoo.co.jp>
2019-05-12 16:46:29 +0900

    slider

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('turbolinks:load',function() {
   var mySwiper = new Swiper('.swiper-container', {
     pagination: {
@@ -15,3 +14,24 @@ document.addEventListener('turbolinks:load',function() {
     centeredSlides:true,
   });
 }, false);
+
+document.addEventListener('turbolinks:load',function() {
+  var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+      el:'.swiper-pagination',
+      clickable: true,
+  },
+    navigation:{
+      nextEl:'.swiper-button-next',
+      prevEl:'.swiper-button-prev',
+    },
+    loop: true,
+    speed: 500,
+    slideToClickedSlide:true,
+    centeredSlides:true,
+  });
+}, false);
+
+setInterval(function(){
+   $('.swiper-button-next').click();
+ },2000);
