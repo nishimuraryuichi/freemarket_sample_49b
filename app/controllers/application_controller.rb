279c392f5152c6eaa9523f506d49590c76f01eb9
q1umi-ooki <q1umi-ooki>
2019-05-14 10:51:53 +0900

    Add-nickname-column-to-users-table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :basic_auth, if: :production?
     protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
     # Payjp.api_key = Rails.application.credentials.payjp[:payjp_private_key]
 
   private
@@ -9,11 +10,14 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
 end
