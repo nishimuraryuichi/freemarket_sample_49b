18073a58d33225aeb53ee773807ffa6d9acc6d10
q1umi-ooki <q1umi-ooki>
2019-05-15 21:04:23 +0900

    add-Facebook-omniauth-authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,52 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
   has_one :profile
   has_many :products
   validates :password, presence: true, length: {minimum:6}, confirmation: true
   validates :nickname ,presence: true, length: {maximum: 30}
+
+
+    #facebookに登録があるメソッドが以下
+  def self.from_omniauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first 
+    #すでにデータベースに該当のuid,providerが存在している facebook認証をしている
+    if snscredential.present? #facebook認証済みで、該当uid＆providerが存在するとき
+      user = User.where(id: snscredential.user_id).first
+        unless user.present? #紐づくユーザーがいない限りにおいて以下を追加して
+          user = User.new(   #そんなことは起こらないけど
+            nickname: auth.info.name,
+            email: auth.info.email
+          )
+          end 
+      sns = snscredential 
+    else #facebook認証がまだの場合。
+      user = User.where(email: auth.info.email).first 
+        if user.present? #メルカリに登録済みだけど,facebook認証がまだ。
+          sns = SnsCredential.new(
+            uid: uid,
+            provider: provider,
+            user_id: user.id
+          )
+        else #メルカリに登録していない
+          user = User.new(
+            nickname: auth.info.name,
+            email: auth.info.email
+          )
+          sns = SnsCredential.create(
+            uid: uid,
+            provider: provider
+          )
+        end
+    end    
+    return {user: user, sns_id: sns.id}
+  end
+
+  
+
 
   def card_number_last4
     if card_num = MyPayjp.default_card_num?(self.id.to_s)
